18c697f5b970b28f7fb1800e84c9c8f6c9aeafea
Shikun Liu <sk.lorenmt@gmail.com>
2020-08-07 13:40:06 +0100

    Update visualise_shape.py

--- visualise_shape.py
+++ visualise_shape.py
@@ -8,14 +8,14 @@ save_path = 'visuals/my_shape.png'
 
 y = 0
 for i in range(len(shape)):
-    scale = shape[i] / 32
+    scale = shape[i] / shape[0]
     x = 0 - (10 * scale) / 2
     rectangle = plt.Rectangle((x, y), 10 * scale, 3, color=(0.70, 0.71, 0.71))
     y -= 4
     plt.gca().add_patch(rectangle)
 plt.axis('off')
 plt.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
-plt.margins(0,0)
+plt.margins(0, 0)
 plt.gca().xaxis.set_major_locator(plt.NullLocator())
 plt.gca().yaxis.set_major_locator(plt.NullLocator())
 plt.savefig('{}'.format(save_path), bbox_inches='tight', pad_inches=0)
